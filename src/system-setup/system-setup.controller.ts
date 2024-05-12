04cfd7a0dbf56f6868f5167e1dc81526c5588b6c
flexiG <mister23krut@gmail.com>
2024-05-12 22:32:07 +0500

    Рассылка по почте

--- src/system-setup/system-setup.controller.ts
+++ src/system-setup/system-setup.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Query, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../guard/roles.guard';
 import { UserRole } from '../enums/role.enum';
@@ -7,15 +7,24 @@ import { Roles } from '../decorators/roles.decorator';
 import { StudentInterface } from '../interfaces/AllStudentsFields.interface';
 import { response } from 'express';
 import { Prisma } from '@prisma/client';
+import { ApiTags } from '@nestjs/swagger';
+import { PageOptionsDto } from "../utils/page/dtos";
+import { CreateStudentDto } from "../students/dto/create-student.dto";
+import { SendMailDto } from "./dto/send-mail.dto";
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('system')
+@ApiTags('system')
 export class SystemSetupController {
   constructor(private readonly systemSetupService: SystemSetupService) {}
-
   @Roles(UserRole.EDITOR)
   @Get('/:column_name')
   findAll(@Query() res: { table: Prisma.ModelName; column: string }) {
     return this.systemSetupService.findColumns(res);
+  }
+
+  @Post('/sendStudent')
+  sendMessage(@Body() sendMailDto: SendMailDto, @Query() res: Response) {
+    return this.systemSetupService.sendMessage(sendMailDto, res);
   }
 }
--- src/system-setup/system-setup.service.ts
+++ src/system-setup/system-setup.service.ts
@@ -1,7 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Query } from "@nestjs/common";
 import { PrismaService } from '../prisma.service';
 import { Prisma } from '@prisma/client';
+import { SendMailDto } from "./dto/send-mail.dto";
+import nodemailer from "nodemailer"
+import { MessageInterface } from "./interfaces/message.interface";
+import { ExceptionHandler } from "@nestjs/core/errors/exception-handler";
+class MailService {
 
+  #transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  })
+
+  async sendEmail(message: MessageInterface) {
+    const mail = {
+      from: `"${message.from}" <${process.env.EMAIL}>`,
+      to: message.to,
+      bcc: process.env.EMAIL,
+      subject: message.subject,
+      text: message.text,
+      html: message.html,
+      attachments: message.files.map(file => new Object({
+        filename: file.originalname,
+        path: file.path
+      }))
+    }
+
+    await this.#transport.sendMail(mail)
+  }
+}
 @Injectable()
 export class SystemSetupService {
   constructor(private prisma: PrismaService) {}
@@ -17,6 +49,16 @@ export class SystemSetupService {
         return response.map((field: any) => {
           return field[res.column];
         });
+      });
+  }
+
+  sendMessage(sendMailDto: SendMailDto, res: Response) {
+    MailService.sendEmail(sendMailDto)
+      .then((data) => {
+        res.status(data.status).json({message: "Письмо отправлено"})
+      })
+      .catch((e) => {
+        throw new ExceptionHandler(e);
       });
   }
 }
